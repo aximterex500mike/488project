570fc0dfdb5776a54aa399e720ca46b3affd5f4d
SmokeySteeplechase <48635844+SmokeySteeplechase@users.noreply.github.com>
2021-02-22 18:35:07 -0500

    Add files via upload
    Weapon/bullet Scripts

---
+++ DoneFor/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+  
+    public Transform Gun;
+
+    Vector2 direction;
+    
+    public GameObject Bullet;
+
+    public float BulletSpeed;
+
+    public Transform ShootPoint;
+
+    public float fireRate;
+
+    float ReadyForNextShot;
+
+
+
+    void Start()
+    {
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+      Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+    direction = (Vector2)mousePos - (Vector2)Gun.position;
+    faceMouse();
+
+    if(Input.GetMouseButton(0)){
+      if(Time.time > ReadyForNextShot)
+      {
+        ReadyForNextShot = Time.time + 1/fireRate;
+        shoot();
+      }
+      
+    }
+
+    }
+
+    void faceMouse()
+    {
+        Gun.transform.right = direction;
+    }
+
+    void shoot()
+    {
+      GameObject BulletIns = Instantiate(Bullet,ShootPoint.position,ShootPoint.rotation);
+      BulletIns.GetComponent<Rigidbody2D>().AddForce(BulletIns.transform.right * BulletSpeed);
+      Destroy(BulletIns,2);
+    }
+
+}
