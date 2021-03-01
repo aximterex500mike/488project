9b4a7c6f44b982ad1112d9ac77474d08a1d974a1
aximterex500mike <michaelnarvaezwork@gmail.com>
2021-03-01 18:44:51 -0500

    Character Movement and Animation

---
+++ DoneFor/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+   public void StartGame() {
+       Application.LoadLevel("Stage1");
+   }
+
+   public void ToSettings() {
+       Application.LoadLevel("SettingsMenu");
+   }
+
+   public void ToLevelSelection() {
+       Application.LoadLevel("LevelSelection");
+   }
+
+   public void ToMainMenu() {
+       Application.LoadLevel("MainMenu");
+   }
+
+   public void ToHighScore() {
+       Application.LoadLevel("HighScoreMenu");
+   }
+}
